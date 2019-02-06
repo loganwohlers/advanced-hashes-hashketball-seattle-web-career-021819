6287dd2af05c8527c285a35ec1c5beac8d043d5b
Logan Wohlers <lawohlers@gmail.com>
2019-02-06 22:44:02 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -15,7 +15,7 @@ def game_hash
              "Mason Plumlee"=>{number: 1 , shoe: 19, points: 26, 
               rebounds: 12, assists: 6, steals: 3, blocks: 8, slam_dunks:
               5},
-             "Jason Terry"=>{number: 21 , shoe: 15, points: 19, 
+             "Jason Terry"=>{number: 31 , shoe: 15, points: 19, 
               rebounds: 2, assists: 2, steals: 4, blocks: 11, slam_dunks:
               1} }
   }, away: {
@@ -24,7 +24,7 @@ def game_hash
     players: {"Jeff Adrien"=>{number: 4 , shoe: 18, points: 10, 
               rebounds: 1, assists: 1, steals: 2, blocks: 7, slam_dunks:
               2},
-             "Bismack Biyombo"=>{number: 0 , shoe: 16, points: 12, 
+             "Bismak Biyombo"=>{number: 0 , shoe: 16, points: 12, 
               rebounds: 4, assists: 7, steals: 7, blocks: 15, slam_dunks:
               10},
              "DeSagna Diop"=>{number: 2 , shoe: 14, points: 24, 
@@ -33,7 +33,7 @@ def game_hash
              "Ben Gordon"=>{number: 8 , shoe: 15, points: 33, 
               rebounds: 3, assists: 2, steals: 1, blocks: 1, slam_dunks:
               0},
-             "Brendan Haywood"=>{number: 3 , shoe: 15, points: 6, 
+             "Brendan Haywood"=>{number: 33 , shoe: 15, points: 6, 
               rebounds: 12, assists: 12, steals: 22, blocks: 5, slam_dunks:
               12}
       
@@ -59,18 +59,88 @@ def good_practices
 end
 #good_practices
  #puts gh[:home][:players]["Brook Lopez"][:points]
-#puts a[0]
+
 def num_points_scored (name)
   gh=game_hash
   gh.each do |team, stats|
     stats[:players].each_key do |nameval|
       if (nameval==name)
-        #want whole method to cancel here and return like java
         return gh[team][:players][nameval][:points]
       end
     end
   end
 end
+
+def shoe_size (name)
+  gh=game_hash
+  gh.each do |team, stats|
+    stats[:players].each_key do |nameval|
+      if (nameval==name)
+        return gh[team][:players][nameval][:shoe]
+      end
+    end
+  end
+end
+
+def team_colors (team)
+  gh=game_hash
+  gh.each do |place, stats|
+    if(stats[:team_name]==team)
+      return gh[place][:colors]
+    end
+  end
+end
+
+def team_names 
+teams=[]
+  gh=game_hash
+  gh.each do| team, stats|
+    teams.push(gh[team][:team_name])
+  end
+  teams
+end
+
+def player_numbers (nbateam)
+  jerseys=[]
+  gh=game_hash
+  gh.each do |team, stats|
+    if stats[:team_name]==nbateam
+      stats[:players].each_key do |name|
+        jerseys.push (gh[team][:players][name][:number])
+      end
+    end
+  end
+  jerseys
+end
+
+def player_stats (name)
+  gh=game_hash
+  gh.each do |team, stats|
+    stats[:players].each_key do |nameval|
+      if (nameval==name)
+        return gh[team][:players][nameval]
+      end
+    end
+  end
+end
+
+def big_shoe_rebounds
+  gh=game_hash
+  bigsize=0
+  bigfoot="name"
+  teamplace=""
+  gh.each do |team, stats|
+    stats[:players].each_key do |nameval|
+        if (gh[team][:players][nameval][:shoe]>bigsize)
+          bigsize=gh[team][:players][nameval][:shoe]
+          bigfoot=nameval
+          teamplace=team
+        end
+    end
+  end
+  gh[teamplace][:players][bigfoot][:rebounds]
+end
+
 
   
 
